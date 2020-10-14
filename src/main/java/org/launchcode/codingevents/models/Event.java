e3f6cda92409b7bb52ab092fa71e1c26c524ce3a
Larry LaMar <lmrlrry@gmail.com>
2020-10-13 23:04:18 -0500

    completed exercise for validation

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,8 +1,6 @@
 package org.launchcode.codingevents.models;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.Objects;
 
 /**
@@ -24,6 +22,15 @@ public class Event {
     @Email(message = "Invalid email. Try again.")
     private String contactEmail;
 
+    @NotBlank(message = "Location is required!")
+    private String location;
+
+    @AssertTrue(message = "Must be checked")
+    private boolean registrationRequired;
+
+    @Min(value = 1, message = "Must be at least one attendee!")
+    private int attendees;
+
     public Event(String name, String description, String contactEmail) {
         this();
         this.name = name;
@@ -34,6 +41,30 @@ public class Event {
     public Event() {
         this.id = nextId;
         nextId++;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean isRegistrationRequired() {
+        return registrationRequired;
+    }
+
+    public void setRegistrationRequired(boolean registrationRequired) {
+        this.registrationRequired = registrationRequired;
+    }
+
+    public int getAttendees() {
+        return attendees;
+    }
+
+    public void setAttendees(int attendees) {
+        this.attendees = attendees;
     }
 
     public String getName() {
